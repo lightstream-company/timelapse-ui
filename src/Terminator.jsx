e2b50a3437c86b9c6e4bf12f45ca850a9652baa0
Franck Ernewein <franck.ernewein@gmail.com>
2016-11-18 16:43:08 +0100

    task(map): draw terminator curve

--- src/Terminator.jsx
+++ src/Terminator.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
+import _ from 'lodash';
 import { project } from 'equirectangular-projection';
-import { normalizeLongitude } from './Geo/normalize';
 import { antipode, solarPosition } from './terminator';
+import * as d3 from 'd3-geo';
 
 
 export default function Terminator(props) {
   const {width, height, time} = props;
 
+  //const solar = [0, 0];
   const solar = solarPosition(time);
   const [lng, lat] = antipode(solar);
-  const {top, left} = project(normalizeLongitude(lng), lat, width);
 
-  const style = Object.assign({
-    fill: 'red'
+  const circleGenerator = d3.geoCircle();
+
+  circleGenerator.center([lng, lat]);
+  circleGenerator.radius(90);
+
+  const circle = circleGenerator();
+
+  const points = _.sortBy(circle.coordinates[0], (coor) => coor[0]).map((coor) => {
+    const {top, left} = project(coor[0], coor[1], width);
+    return left + ' ' + top;
   });
+  points.unshift('0 0');
+  points.unshift(width + ' 0');
 
   return <svg {...{
       width,
@@ -22,6 +33,8 @@ export default function Terminator(props) {
         position: 'absolute'
       }
     }}>
-    <rect width="10" height="10" x={left} y={top} style={style} />
+    <polygon points={points.join(', ')} style={{
+      opacity: 0.3
+    }}/>
   </svg>;
 }
