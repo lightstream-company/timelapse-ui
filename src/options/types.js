07a45c561c430dfd05780e3200ca7440c5c5e0b8
Franck Ernewein <franck.ernewein@gmail.com>
2016-12-20 10:39:51 +0100

    refactor(options): use native HexColor from tcomb

--- src/options/types.js
+++ src/options/types.js
@@ -1,12 +1,6 @@
 import t from 'tcomb';
 import * as tt from 'tcomb-form-types';
 
-function checkColorLength(str) {
-  return str.length === 6 || str.length === 3;
-}
-
-export const ColorCode = t.refinement(tt.String.AlphaNumeric, checkColorLength, 'ColorCode');
-
 export const Options = t.struct({
-  bgColor: ColorCode
+  bgColor: tt.String.HexColor
 });
